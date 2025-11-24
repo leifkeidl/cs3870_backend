f95317c23a12134f8ead3ac17311acf1442cb5e0
Leif Keidl <keidll@uwplatt.edu>
2025-11-24 11:30:03 -0600

    Clean initial commit

---
+++ index.js
@@ -0,0 +1,190 @@
+import { MongoClient } from "mongodb";
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+
+
+// Create Express app
+const app = express();
+// Middleware
+app.use(cors());
+app.use(express.json()); // replaces body-parser
+
+
+// Start server
+app.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
+});
+
+// MongoDB
+// Server configuration
+const PORT = process.env.PORT ?? 8081;
+const HOST = process.env.HOST ?? "0.0.0.0";
+// MongoDB configuration
+const MONGO_URI = process.env.MONGO_URI;
+const DBNAME = process.env.DBNAME;
+const collection = process.env.COLLECTION;
+const client = new MongoClient(MONGO_URI);
+const db = client.db(DBNAME);
+
+app.get("/contacts", async (req, res) => {
+
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
+
+    const query = {};
+    const results = await db
+        .collection(collection)
+        .find(query)
+        .limit(100)
+        .toArray();
+    console.log(results);
+
+    res.status(200);
+    // res.send(results);
+    res.json(results);
+
+
+});
+
+app.post("/contacts", async (req, res) => {
+
+    try {
+
+        // The body exists
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).send({ message: 'Bad request: No data provided.' });
+        }
+
+        // Extract fields from body
+        const { contact_name, phone_number, message, image_url } = req.body;
+
+        // Connect to MongoDB
+        await client.connect();
+        console.log("Node connected successfully to POST MongoDB");
+
+        // Reference collection
+        const contactsCollection = db.collection(collection);
+
+        // Check if contact already exists
+        const existingContact = await contactsCollection.findOne({
+            contact_name: contact_name,
+        });
+
+        if (existingContact) {
+            return res.status(409).json({
+                message: `Contact with name '${contact_name}' already exists.`,
+            });
+        }
+
+        // Create new Document to POST
+        const newDocument = {
+            contact_name,
+            phone_number,
+            message,
+            image_url,
+        };
+        console.log(newDocument);
+
+        // Insert new document into MongoDB
+        const result = await contactsCollection.insertOne(newDocument);
+        console.log("Document inserted:", result);
+
+        // Acknowledge frontend
+        res.status(201);
+        res.json({ message: "New contact added successfully" });
+
+    } catch (error) {
+        console.error("Error in POST /contacts:", error);
+        res.status(500);
+        res.json({ message: "Failed to add contact: " + error.message });
+    } finally {
+        await client.close();
+    }
+});
+app.post("/update", async (req, res) => {
+
+    try {
+
+        // The body exists
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).send({ message: 'Bad request: No data provided.' });
+        }
+
+        // Extract fields from body
+        const { contact_name, phone_number, message, image_url } = req.body;
+
+        // Connect to MongoDB
+        await client.connect();
+        console.log("Node connected successfully to POST MongoDB");
+
+        // Reference collection
+        const contactsCollection = db.collection(collection);
+
+
+        // Create new Document to POST
+        const newDocument = {
+            contact_name,
+            phone_number,
+            message,
+            image_url,
+        };
+        console.log(newDocument);
+
+        // Insert new document into MongoDB
+        const result = await contactsCollection.updateOne({ contact_name: contact_name }, { $set: { phone_number: phone_number, message: message, image_url: image_url } });
+
+        console.log("Document inserted:", result);
+
+        // Acknowledge frontend
+        res.status(201);
+        res.json({ message: "New contact added successfully" });
+
+    } catch (error) {
+        console.error("Error in POST /contacts:", error);
+        res.status(500);
+        res.json({ message: "Failed to add contact: " + error.message });
+    } finally {
+        await client.close();
+    }
+});
+
+
+app.delete("/contacts/:name", async (req, res) => {
+    try {
+        // Read parameter id
+        const name = req.params.name;
+        console.log("Contact to delete :", name);
+        // Connect to MongoDB
+        await client.connect();
+        console.log("Node connected successfully to POST MongoDB");
+        // Reference collection
+        const contactsCollection = db.collection(collection);
+        // Check if contact already exists
+        const existingContact = await contactsCollection.findOne({
+            contact_name: name,
+        });
+        if (!existingContact) {
+            return res.status(404).json({
+                message: `Contact with name ${name} does NOT exist.`,
+            });
+        }
+        // Define query
+        const query = { contact_name: name };
+        // Delete one contact
+        const results = await db.collection("contacts").deleteOne(query);
+        // Response to Client
+        res.status(200);
+        // res.send(results);
+        res.send({ message: `Contact ${name} was DELETED successfully.` });
+    }
+    catch (error) {
+        console.error("Error deleting robot:", error);
+        res.status(500).send({ message: 'Internal Server Error' + error });
+    }
+});
+
